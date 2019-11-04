f93cec2b64d958667c61e120457494087dc672ef
Sokratis Zappis <szappis@redhat.com>
2019-11-04 17:06:54 +0100

    Remove PyYAML step from db modules

--- tomcat9/custom-modules/install-dbs-jdk8/dbs-jdk8.sh
+++ tomcat9/custom-modules/install-dbs-jdk8/dbs-jdk8.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
 
-yum --setopt=tsflags=nodocs install -y rh-mongodb36-mongo-java-driver postgresql-jdbc mysql-connector-java PyYAML \
-     && rpm -q rh-mongodb36-mongo-java-driver postgresql-jdbc mysql-connector-java PyYAML
-
+yum --setopt=tsflags=nodocs install -y rh-mongodb36-mongo-java-driver postgresql-jdbc mysql-connector-java \
+     && rpm -q rh-mongodb36-mongo-java-driver postgresql-jdbc mysql-connector-java
