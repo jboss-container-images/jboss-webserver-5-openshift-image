b97c82791a0a8a3fdb12a66b54192a292bcd86f5
Sokratis Zappis <szappis@redhat.com>
2020-01-13 11:13:45 +0100

    Use mysql connector 8.0.18 from mysql.com

--- tomcat9/custom-modules/install-dbs-jdk11/dbs-jdk11.sh
+++ tomcat9/custom-modules/install-dbs-jdk11/dbs-jdk11.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-yum --setopt=tsflags=nodocs install -y rh-mongodb36-mongo-java-driver postgresql-jdbc mysql-connector-java \
-     && rpm -q rh-mongodb36-mongo-java-driver postgresql-jdbc mysql-connector-java
+yum --setopt=tsflags=nodocs install -y rh-mongodb36-mongo-java-driver postgresql-jdbc \
+     && rpm -q rh-mongodb36-mongo-java-driver postgresql-jdbc
+rpm -Uvh /tmp/artifacts/mysql.rpm
 #The dbs pull openjdk8 so we have to remove it
-rpm -e --nodeps java-1.8.0-openjdk-headless java-1.8.0-openjdk
+rpm -e --nodeps java-1.8.0-openjdk-headless
